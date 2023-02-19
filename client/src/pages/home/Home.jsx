7b7e5c029d9fd37fa1f4cb30864f83494a984ea9
Ankur982 <singh999ankur@gmail.com>
2023-02-19 17:43:51 +0530

    adding home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import { Box, Button, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
 
 export const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Box
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      gap={"50px"}
+      width={"88%"}
+      m={"auto"}
+    >
+      <Box textAlign={"left"}>
+        <Box>
+          <Heading as="h2" size="3xl" noOfLines={2}>
+            Evolving Technology for evolving times
+          </Heading>
+          <Text fontSize="2xl" mt={"25px"}>
+            Experience the ease of reconciling your data with our reconciliation
+            software.
+          </Text>
+        </Box>
+        <Box display={"flex"} gap={"50px"} mt={"50px"}>
+          <Button colorScheme="blackAlpha" size="lg">
+            Fetch Users
+          </Button>
+          <Button colorScheme="blackAlpha" size="lg">
+            Delete Users
+          </Button>
+          <Button colorScheme="blackAlpha" size="lg">
+            User Details
+          </Button>
+        </Box>
+      </Box>
+      <Box display={"block"} m={"auto"}>
+        <Image
+          src={
+            "https://www.cointab.in/wp-content/uploads/2022/09/Untitled-design-31.png"
+          }
+          alt={"img"}
+        />
+      </Box>
+    </Box>
+  );
+};
